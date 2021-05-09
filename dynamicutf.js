ae41fd239b308b72c30721293816fe12e9711175
AbodyRulez <abdalla.m.magdy@gmail.com>
2021-05-09 20:01:24 +0300

    Create dynamicutf.js

---
+++ dynamicutf.js
@@ -0,0 +1,25 @@
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+
+<script>
+  
+function getURLParameter(name) {
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split("=");
+        if (pair[0] === name) {
+            return pair[1];
+        }
+    }
+    return "";
+}
+
+$(document).ready(function () {
+    var source = getURLParameter("utm_source");
+		var name = document.getElementById("bundlename").textContent || "%D8%A7%D9%84%D8%B9%D9%86%D8%A7%D9%8A%D8%A9%20%D8%A8%D8%A7%D9%84%D8%B5%D8%AD%D8%A9";
+  
+  	var url = `https://get.cura.healthcare/chat/?bundle=${name}&utm_source=${source}`;
+  	$(".source-btn").attr("href", url);
+});
+
+</script>
